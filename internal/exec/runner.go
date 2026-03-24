0a513b7a9133f3c5c36f76ed948d76ed1db07eab
Ivan M <cartman.dev@gmail.com>
2026-03-24 19:47:53 +0100

    feat: add command runner with dry-run and verbose support

---
+++ internal/exec/runner.go
@@ -0,0 +1,49 @@
+package exec
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type Runner struct {
+	DryRun  bool
+	Verbose bool
+	Output  io.Writer
+}
+
+func NewRunner(dryRun, verbose bool) *Runner {
+	return &Runner{
+		DryRun:  dryRun,
+		Verbose: verbose,
+		Output:  os.Stderr,
+	}
+}
+
+func (r *Runner) Run(name string, args ...string) (string, error) {
+	cmdStr := name + " " + strings.Join(args, " ")
+
+	if r.DryRun {
+		fmt.Fprintf(r.Output, "  [dry-run] %s\n", cmdStr)
+		return "", nil
+	}
+
+	if r.Verbose {
+		fmt.Fprintf(r.Output, "  $ %s\n", cmdStr)
+	}
+
+	cmd := exec.Command(name, args...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("%s: %s", cmdStr, strings.TrimSpace(stderr.String()))
+	}
+
+	return strings.TrimSpace(stdout.String()), nil
+}
